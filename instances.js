3e1757841a57dce77a98c1add9ca643ee91c257b
gpokshit <127311197+gpokshit@users.noreply.github.com>
2023-03-08 16:08:42 +0300

    Add tabs

--- instances.js
+++ instances.js
@@ -1,9 +1,9 @@
 const instances = [
-'invidious.snopyta.org',
-'invidious.sethforprivacy.com',
-'y.com.sb',
-'invidious.baczek.me',
-'yewtu.be',
-'watch.thekitty.zone',
-'vid.puffyan.us'
+	'invidious.snopyta.org',
+	'invidious.sethforprivacy.com',
+	'y.com.sb',
+	'invidious.baczek.me',
+	'yewtu.be',
+	'watch.thekitty.zone',
+	'vid.puffyan.us'
 ]
